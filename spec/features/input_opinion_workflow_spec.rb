eb73b10a8eb73229b8089fc15edf6138640fe05b
Ta Minh Tien <taminhtien1993@gmail.com>
2015-11-04 11:30:23 +0700

    Update feature test for opinion work flow

---
+++ spec/features/input_opinion_workflow_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe 'Input Opinion Workflow', type: :feature do
+	def fill_in_the_form
+		fill_in 'Content', with: opinion
+		choose 'Yes'
+		click_on 'Create Opinion'
+	end
+
+	let!(:topics) do
+		[
+			create(:topic, content: 'Is Ruby better than Python?'),
+			create(:topic, content: 'Should abortion be legalized?'),
+			create(:topic, content: 'Is SASS better than LESS'),
+		]
+	end
+	let(:opinion) { 'Ruby is better!' }
+
+	it 'allows user to input his opinion' do
+		visit '/'
+		expect(page).to have_content topics.first.content
+
+		fill_in_the_form
+		expect(page).to have_content topics.second.content
+
+		fill_in_the_form
+		expect(page).to have_content topics.last.content
+	end
+end
